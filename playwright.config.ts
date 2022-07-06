975406dd36f25d7d908c735b5db456905cfea1fe
Lucas Donato <ldonato@gattecnologia.com.br>
2022-07-06 13:54:16 -0300

    add backup bd

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,7 @@ const config: PlaywrightTestConfig = {
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 1,
     testDir: 'tests-api/',
+    //workers: 1,
     use: {
         screenshot: "only-on-failure",
         extraHTTPHeaders: {
