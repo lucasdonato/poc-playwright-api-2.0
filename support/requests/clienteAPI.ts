7c715759f3ab39f00c62245995d3db0cf8339408
Lucas Donato <ldonato@gattecnologia.com.br>
2022-07-11 13:37:59 -0300

    add more tests

--- support/requests/clienteAPI.ts
+++ support/requests/clienteAPI.ts
@@ -5,10 +5,9 @@ const endpoint = process.env.URL_BASE + '/clientes'
 export class reqCliente {
 
     static async post(cliente: Object, request: any) {
-
-        const response = await request.post(endpoint, { data: cliente });
+        const response = await request.post(endpoint, { data: cliente, });
         const responseBody = JSON.parse(await response.text())
 
-        return { body: responseBody, status: response.status() }
+        return { body: responseBody, status: response.status(), headers: response.headers() }
     }
 }
